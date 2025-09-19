700f61d04f7ad68b8d02224af484b7555f54b874
abstruse-scientia <priyanshu0496@gmail.com>
2025-09-20 02:27:12 +0530

    feat: setup backend project structure

---
+++ backend/mercatus/src/main/java/com/scientia/mercatus/MercatusApplication.java
@@ -0,0 +1,13 @@
+package com.scientia.mercatus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercatusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercatusApplication.class, args);
+	}
+
+}
---
+++ backend/mercatus/src/test/java/com/scientia/mercatus/MercatusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scientia.mercatus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercatusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
