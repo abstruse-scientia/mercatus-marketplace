8f33d560a769b82753b683823ed6e417f08d3467
abstruse-scientia <priyanshu0496@gmail.com>
2026-02-16 01:25:42 +0530

    fix: remove duplicate migration file.V11 duplicate of V10

--- backend/mercatus/src/main/resources/db/migrations/V11__ensure_default_active_user_column_and_index.sql
+++ backend/mercatus/src/main/resources/db/migrations/V11__ensure_default_active_user_column_and_index.sql
@@ -1,15 +1,2 @@
-ALTER TABLE address
-    DROP COLUMN default_active_user;
-
-ALTER TABLE address
-    ADD COLUMN default_active_user BIGINT
-        GENERATED ALWAYS AS (
-            CASE
-                WHEN is_default = TRUE AND is_active = TRUE
-                    THEN user_id
-                ELSE NULL
-                END
-            ) STORED;
-
-CREATE UNIQUE INDEX uq_idx_user_default_address
-    ON address (default_active_user);
+-- This migration is intentionally kept empty to maintain migration history
+-- V10 already takes care of default_active-user column and uq_index_default address index.
